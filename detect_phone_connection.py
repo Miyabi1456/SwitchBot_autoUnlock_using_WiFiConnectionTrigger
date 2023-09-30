05a3aab46f23d8dcb61eb48e1f9c8fd966b07a52
Miyabi1456 <40091129+Miyabi1456@users.noreply.github.com>
2023-09-30 12:15:53 +0900

    Update detect_phone_connection.py

--- detect_phone_connection.py
+++ detect_phone_connection.py
@@ -3,12 +3,11 @@ host = "" #スマートフォンのIPアドレスを入力
 
 def detect_connection():
     connected = False
-    send_num = 2
     timeout = 2.0 #s
     
-    rcv = uping.ping(host, count=send_num, timeout=timeout*1000, interval=10, quiet=False, size=64)
+    send, rcv = uping.ping(host, count=5, timeout=timeout*1000, interval=10, quiet=False, size=64)
     
-    if rcv == (send_num,send_num):
+    if rcv > 0: # 1つでも届けば接続状態とする
         print("スマホ接続")
         connected = True
     else:
